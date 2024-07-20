46643aebc595201fc350ef1cd1717aced1013b95
mobin-em <mirshafie.mohamadreza@gmail.com>
2024-07-20 08:31:45 +0330

    Update index.php

--- index.php
+++ index.php
@@ -9,21 +9,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    // Create the Transport
     $transport = (new Swift_SmtpTransport('smtp.gmail.com', 587, 'tls'))
-        ->setUsername('mirshafie.mohamadreza@gmail.com') // آدرس ایمیل شما
+        ->setUsername('mirshafie.mohamadreza@gmail.com') // آدرس ایمیل
         ->setPassword('wwsn qohs maqv vguy'); // رمز عبور برنامه
-
-    // Create the Mailer using your created Transport
+   
     $mailer = new Swift_Mailer($transport);
 
-    // Create a message
     $emailMessage = (new Swift_Message('پیام جدید از فرم تماس'))
         ->setFrom(['mohamadreza819596@gmail.com' => 'mohamadreza']) // آدرس ایمیل و نام فرستنده
         ->setTo(['mirshafie.mohamadreza@gmail.com']) // آدرس ایمیل مقصد
         ->setBody("نام: $name<br>ایمیل: $email<br><br>پیام:<br>$message", 'text/html');
-
-    // Send the message
+    
     try {
         $result = $mailer->send($emailMessage);
         echo 'پیام شما با موفقیت ارسال شد.';

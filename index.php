4181e59304ca616fab43a1b423956bc44a7daaf3
mobin-em <mirshafie.mohamadreza@gmail.com>
2024-07-21 09:19:17 +0330

    Add files via upload

--- index.php
+++ index.php
@@ -4,27 +4,41 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once 'vendor/autoload.php';
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    $transport = (new Swift_SmtpTransport('smtp.gmail.com', 587, 'tls'))
-        ->setUsername('mirshafie.mohamadreza@gmail.com') // آدرس ایمیل
-        ->setPassword('wwsn qohs maqv vguy'); // رمز عبور برنامه
-   
-    $mailer = new Swift_Mailer($transport);
+    // Create a new PHPMailer instance
+    $mail = new PHPMailer(true);
 
-    $emailMessage = (new Swift_Message('پیام جدید از فرم تماس'))
-        ->setFrom(['mohamadreza819596@gmail.com' => 'mohamadreza']) // آدرس ایمیل و نام فرستنده
-        ->setTo(['mirshafie.mohamadreza@gmail.com']) // آدرس ایمیل مقصد
-        ->setBody("نام: $name<br>ایمیل: $email<br><br>پیام:<br>$message", 'text/html');
-    
     try {
-        $result = $mailer->send($emailMessage);
+        //Server settings
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'mirshafie.mohamadreza@gmail.com'; // آدرس ایمیل شما
+        $mail->Password   = 'wwsn qohs maqv vguy'; // رمز عبور برنامه
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
+
+        //Recipients
+        $mail->setFrom('mohamadreza819596@gmail.com', 'mohamadreza'); // آدرس ایمیل و نام فرستنده
+        $mail->addAddress('mirshafie.mohamadreza@gmail.com'); // آدرس ایمیل مقصد
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'پیام جدید از فرم تماس';
+        $mail->Body    = "نام: $name<br>ایمیل: $email<br><br>پیام:<br>$message";
+
+        // Send the message
+        $mail->send();
         echo 'پیام شما با موفقیت ارسال شد.';
     } catch (Exception $e) {
-        echo "مشکلی در ارسال پیام رخ داده است. لطفا دوباره تلاش کنید. خطا: {$e->getMessage()}";
+        echo "مشکلی در ارسال پیام رخ داده است. لطفا دوباره تلاش کنید. خطا: {$mail->ErrorInfo}";
     }
 }
 ?>
